13a94f34d2d5641ffdd1f7238fa2a815d0429680
Muamer Kukic <kukicako@gmail.com>
2019-11-11 09:35:55 -0800

    commit

--- api/api-router.js
+++ api/api-router.js
@@ -5,9 +5,23 @@ const usersRouter = require('../users/users-router.js');
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
+const bcrypt = require('bcryptjs');
+
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  const  pw = req.body.password;
+
+  //hash pw
+
+  req.body.password = bcrypt.hashSync(pw, 12);
+
+  //return to user in an object
+
+  res.status(200).json({password: `${pw}`, hashed: `${req.body.password}`});
+})
 
 module.exports = router;
